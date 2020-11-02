0b4839c3f7eea4285cf7eda49c730c14a1e59cb8
AdamSchnapp <adschnapp@gmail.com>
2020-11-02 15:16:56 -0500

    return ds when wrangling gfs meta

--- camps/wrangle.py
+++ camps/wrangle.py
@@ -13,3 +13,4 @@ def friendly_gfs(ds: xr.Dataset) -> xr.Dataset:
     ds = ds.set_coords(['longitude', 'latitude'])
     ds = ds.rename_dims({'default_time_coordinate_size': 'reference_time', 'lead_times': 'lead_time'})
     ds = ds.set_index({'reference_time':'FcstRefTime', 'lead_time':'LeadTime' })
+    return ds
